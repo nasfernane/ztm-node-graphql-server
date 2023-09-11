438832cd683f557f61d83b73b60a938ec8b7eb8f
Nassim Fernane <nasfernane@gmail.com>
2023-09-11 23:37:17 +0200

    initial commit server base with graphql-http library to build schema

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { createHandler } = require('graphql-http/lib/use/express');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLFloat } = require('graphql');
+
+// using deprecated library express-graphql
+// const { graphqlHTTP } = require('express-graphql')
+
+const PORT = 3000;
+
+// using deprecated library express-graphql
+// const schema = buildSchema(`
+//   type Query {
+//     description: String
+//     price: Float
+//   } 
+// `)
+
+const root = {
+  description: 'Red shoe',
+  price: 42.12,
+}
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      description: {
+        type: GraphQLString,
+      },
+      price: {
+        type: GraphQLFloat,
+      }
+    },
+  })
+})
+
+const app = express();
+
+app.post('/graphql', createHandler({schema, rootValue: root}));
+
+// using deprecated library express-graphql
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+// }));
+
+app.listen(PORT, () => {
+  console.log(`Running GraphQL server on port ${PORT}`)
+})
