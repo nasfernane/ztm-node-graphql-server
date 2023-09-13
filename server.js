0390325f3aaf6c4f4651460dbd2f7297801a10bc
Nassim Fernane <nasfernane@gmail.com>
2023-09-13 23:06:18 +0200

    simulation bdd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createHandler } = require('graphql-http/lib/use/express');
 const expressPlayground = require('graphql-playground-middleware-express').default
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLFloat } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLFloat, GraphQLList, GraphQLNonNull } = require('graphql');
 
 // using deprecated library express-graphql
 // const { graphqlHTTP } = require('express-graphql')
@@ -17,22 +17,113 @@ const PORT = 3000;
 // `)
 
 const root = {
-  description: 'Red shoe',
-  price: 42.12,
+  products: [
+    {
+      id: 'redshoe',
+      description: 'Red Shoe',
+      reviews: [
+        { 
+          rating: 4,
+          comment: 'blabla'
+        }
+      ],
+      price: 14.7,
+    },
+    {
+      id: 'bluejean',
+      description: 'Blue Jeans',
+      price: 58.50,
+    }
+  ],
+  orders: [
+    {
+      date: '2005-05-05',
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'Old Red Shoe',
+            price: 45.11
+          },
+          quantitu: 2
+        }
+      ]
+    }
+  ]
 }
+
+const ReviewType = new GraphQLObjectType({
+  name: 'Review',
+  fields: {
+    rating: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    comment: {
+      type: GraphQLString
+    },
+  }
+})
+
+const ProductType = new GraphQLObjectType({
+  name: 'Product',
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
+    description: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    reviews: {
+      type: new GraphQLList(ReviewType)
+    },
+    price: {
+      type: new GraphQLNonNull(GraphQLFloat),
+    }
+  }
+})
+
+
+const OrderItemType  = new GraphQLObjectType({
+  name: 'OrderType',
+  fields: {
+    product: {
+      type: new GraphQLNonNull(ProductType),
+    },
+    quantity: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+   
+  }
+})
+
+const OrderType = new GraphQLObjectType({
+  name: 'Order',
+  fields: {
+    date: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    subtotal: {
+      type: new GraphQLNonNull(GraphQLFloat),
+    },
+    items: {
+      type: new GraphQLList(OrderItemType)
+    }
+  }
+})
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-      description: {
-        type: GraphQLString,
+      products: {
+        type: new GraphQLList(ProductType)
       },
-      price: {
-        type: GraphQLFloat,
+      orders: {
+        type: new GraphQLList(OrderType)
       }
-    },
-  })
+    }
+  }),
 })
 
 const app = express();
