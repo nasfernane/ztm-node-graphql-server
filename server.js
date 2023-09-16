ef627a59d2b9f08c6a37564767725c18b2eaa8ae
Nassim Fernane <nasfernane@gmail.com>
2023-09-16 17:45:19 +0200

    transition vers apollo server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { createHandler } = require('graphql-http/lib/use/express');
-const expressPlayground = require('graphql-playground-middleware-express').default
+const { ApolloServer } = require('apollo-server-express')
 
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
@@ -8,30 +7,27 @@ const { makeExecutableSchema } = require('@graphql-tools/schema');
 const PORT = 3000;
 
 // charge tous les fichiers graphql contenus dans des (sous)répertoires
-const typesArray = loadFilesSync('**/*', {
-  extensions: ['graphql'],
-});
+const typesArray = loadFilesSync('**/*', { extensions: ['graphql'] });
+const resolversArray = loadFilesSync('**/*', { extensions: ['resolvers.js'] });
 
-const resolversArray = loadFilesSync('**/*', {
-  extensions: ['resolvers.js'],
-});
+async function startApolloServer() {
+  const app = express();
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray, 
-  resolvers: resolversArray
-})
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray, 
+    resolvers: resolversArray
+  })
 
-const app = express();
+  const server = new ApolloServer({
+    schema: schema,
+  })
 
-// endpoint principal
-app.post('/graphql', createHandler({
-  schema, 
-}));
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql'})
 
-// endpoint bac à sable pour tester les requêtes
-app.get('/graphiql', expressPlayground({ endpoint: '/graphql' }))
+  app.listen(PORT, () => {
+    console.log(`Running GraphQL server http://www.localhost:${PORT}/graphql`)
+  });
+}
 
-
-app.listen(PORT, () => {
-  console.log(`Running GraphQL server http://www.localhost:${PORT}/graphiql`)
-})
+startApolloServer();
